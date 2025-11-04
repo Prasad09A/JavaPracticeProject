0ef6c54e45e2265364112bc66f776552ccfa0555
Prasad A <prasadamilkanthwar2@gmail.com>
2025-11-05 01:31:48 +0530

    added feature1

--- src/JAVA_8/StreamAPI/PipelineOfOperationsExample.java
+++ src/JAVA_8/StreamAPI/PipelineOfOperationsExample.java
@@ -7,6 +7,6 @@ public class PipelineOfOperationsExample {
     public static void main(String[] args) {
         List<String> names= Arrays.asList("Prasad", "Vikas","ram","omkar","omkar","abhimanyu");
         names.stream().filter(name->name.length()>3).distinct().forEach(i->System.out.println(i));
-System.out.println("Adding remote to local example");
+System.out.println("Adding new line in feature 1");
     }
 }
